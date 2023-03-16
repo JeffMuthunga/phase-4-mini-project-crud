6674a2d3260d3e25d7f22b4b562760f80e324db8
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-16 15:19:43 +0300

    x Please enter the commit message for your changes. Lines starting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
+
+  private
+  def render_not_found_error
+    render json: {error: "#{controller_name.classify} not found"}, status: 404
+  end
 
 end
